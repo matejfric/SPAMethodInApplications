97d45ed5ce5b507b028f6bbeaec6fc207c792e1b
matejfric <matejfric@centrum.cz>
2023-02-14 16:05:53 +0100

    testing, minor changes, support for multi-class classification

--- ProgramFiles/generate_synthetic_problem.m
+++ ProgramFiles/generate_synthetic_problem.m
@@ -1,6 +1,7 @@
-function [X,Y,C_true,Gamma_true,Lambda_true] = generate_problem(T)
-%GENERATE_PROBLEM Summary of this function goes here
-%   Detailed explanation goes here
+function [X,Y,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem(T)
+%GENERATE_PROBLEM
+
+rng(42);
 
 C_true = ...
     [ 1 0 1 0; ...
@@ -8,6 +9,21 @@ C_true = ...
       1 1 -2 0; ...
       0 0 5 1; ...
       0 -1 2 1];
+  
+% C_true = ...
+% [ 10 7 13 25; ...
+%   117 -42 39 -2; ...
+%   11 8 -27 0; ...
+%   90 2 5 36; ...
+%   9 -10 27 77];
+
+K = 4; % clusters
+c = 3; % classes
+
+C_true = randi([-10,10],10,K);
+% r = randi([1 c],1,K);
+% Lambda_true = bsxfun(@eq, r(:), 1:c)';
+% C_true = normalize(C_true);
 
 [D,K] = size(C_true);
   
@@ -17,11 +33,11 @@ for k = 1:K
     Gamma_true(k,idx == k) = 1; % random Gamma
 end
 
-Lambda_true = [1 0 0 0.5;...
-               0 1 0 0.5; ...
+Lambda_true = [1 0 0 1;...
+               0 1 0 0; ...
                0 0 1 0];
 
-Y = Lambda_true*Gamma_true;
+Y = Lambda_true*Gamma_true; % PiY
 
 X = C_true*Gamma_true;
            
--- test_synthetic.m
+++ test_synthetic.m
@@ -1,19 +1,20 @@
 clear all
+close all
 
-addpath('Dataset0')
 addpath('ProgramFiles')
 addpath('ProgramFiles/TQDM') % Progress bar
 addpath('ProgramFiles/AdamarFmincon') % adamar_predict()
 addpath('ProgramFiles/AdamarKmeans') % adamar_kmeans
 addpath('ProgramFiles/SPG') 
 
-T = 100;
-[X,Y,C_true,Gamma_true,Lambda_true] = generate_problem(T);
+T = 1000;
+[X_true,Y_true,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem(T);
 
-sigma = 0; % noise parameter
-X = X + sigma*randn(size(X));
+sigma = 0.15; % noise parameter
+X = X_true + sigma * randn(size(X_true));
 
-nwrong = floor(0.2*T);
+Y = Y_true;
+nwrong = floor(0.05*T); % wrong labels
 idx = randperm(T);
 for i=1:nwrong
   Y(1:2,idx(i)) = Y(2:-1:1,idx(i));
@@ -23,6 +24,8 @@ maxIters = 30;
 Ks = size(C_true,2);
 
 alphas = 0:0.05:1;
+alphas = 0.05:0.05:0.95;
+Ls  = zeros(numel(alphas),length(Ks));
 L1s = zeros(numel(alphas),length(Ks));
 L2s = zeros(numel(alphas),length(Ks));
 
@@ -35,6 +38,11 @@ for idx_alpha=1:length(alphas)
             [Lambda, C, Gamma, stats_train, L_out, PiX] = adamar_kmeans(X', Y, K, alpha, maxIters);
 %            [C, Gamma, PiX, Lambda, it, Lit, learningErrors, stats, L_out] = adamar_fmincon(X', Y, K, alpha, maxIters);
 
+%             [prediction, ~] = find(round(Lambda * Gamma));
+%             [ground_truth, ~] = find(Y);
+%             fprintf("f1score: %.2f", statistics_multiclass(prediction, ground_truth).f1score);
+
+            Ls(idx_alpha,idx_K)  = L_out.L;
             L1s(idx_alpha,idx_K) = L_out.L1;
             L2s(idx_alpha,idx_K) = L_out.L2;
         end
@@ -52,6 +60,26 @@ for idx_K=1:length(Ks)
         text(L1s(i),L2s(i),['$\alpha = ' num2str(alphas(i)) '$'],'Interpreter','latex')
     end
     xlabel('$L_1$','Interpreter','latex')
+    ylabel('$L_2$','Interpreter','latex')
+    hold off
+end
+
+for idx_K=1:length(Ks)
+    figure
+    subplot(1,3,1)
+    hold on
+    plot(alphas,Ls(:, idx_K),'r*-')
+    xlabel('$\alpha$','Interpreter','latex')
+    ylabel('$L$','Interpreter','latex')
+    subplot(1,3,2)
+    hold on
+    plot(alphas,L1s(:, idx_K),'b*-')
+    xlabel('$\alpha$','Interpreter','latex')
+    ylabel('$L_1$','Interpreter','latex')
+    subplot(1,3,3)
+    hold on
+    plot(alphas,L2s(:, idx_K),'m*-')
+    xlabel('$\alpha$','Interpreter','latex')
     ylabel('$L_2$','Interpreter','latex')
     hold off
 end
