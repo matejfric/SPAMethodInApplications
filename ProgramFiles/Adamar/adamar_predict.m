15df53dbd8539a22517c5efa45431455f5711142
matejfric <matejfric@centrum.cz>
2023-03-13 17:55:27 +0100

    rename + changes in parameters

--- ProgramFiles/Adamar/adamar_predict.m
+++ ProgramFiles/Adamar/adamar_predict.m
@@ -1,4 +1,4 @@
-function [stats] = adamar_predict_mat(Lambda, C, K, alpha, a, b, ca_Y, dataset, display)
+function [stats] = adamar_predict(Lambda, C, K, alpha, ca_Y, dataset, display)
 %ADAMAR_PREDICT Make a prediction based on ADAMAR model
 %   Lambda...transion matrix
 %   C........centroids
@@ -7,35 +7,13 @@ function [stats] = adamar_predict_mat(Lambda, C, K, alpha, a, b, ca_Y, dataset, 
 %   b........colmax of X
 %   ca_Y.....matrices of descriptors in cell array
 arguments
-    Lambda, C, K, alpha, a, b, ca_Y, dataset, display = true
+    Lambda, C, K, alpha, ca_Y, dataset, display = true
 end
 
 n = numel(ca_Y);
-
 for i = 1:n
-    %Y = ca_Y;
     Y = ca_Y{i}.X;
     
-    % % Normalization
-    %Y(:,1:end-1) = normalize(Y(:,1:end-1));
-    
-    % MinMaxScaling
-    if ~isempty(a) && ~isempty(b)
-        % Y = rescale(Y,'InputMin',a,'InputMax',b);  % [0,1]
-        
-        % % Normalization
-        %Y(:,1:end-1) = normalize(Y(:,1:end-1));
-        
-        % Selective MinMaxScaling [-1,1]
-        l = -1;
-        u = 1;
-        cols = b > 1; % Select columns to be scaled
-        Y(:,cols) = l + ...
-            ((Y(:,cols)-a(cols))./(b(cols)-a(cols))).*(u-l);
-    end
-    
-    % Is the evaluation process correct???
-
     % K-means (one step)
     dist_from_C = zeros(K, size(Y,1));
     for k=1:K
@@ -46,8 +24,9 @@ for i = 1:n
     for k = 1:K
        GammaY(k,idxY==k) = 1; 
     end
+    
     %PiY = round(Lambda*GammaY)'; % round => binary matrix
-    PiY = (Lambda*GammaY)'; % round => binary matrix
+    PiY = (Lambda*GammaY)';
 
     % Statistics
     stats = statistics(PiY(:,1), Y(:,end));
@@ -66,6 +45,9 @@ for i = 1:n
     elseif strcmp(dataset, 'Dataset256')
         original_rgb{1} = imread(sprintf('Dataset/Original256/%d.jpg', ca_Y{i}.I));
         annnotation{1} = imread(sprintf('Dataset/Annotations256/%d.png', ca_Y{i}.I));
+    elseif strcmp(dataset, 'DatasetSelection')
+        original_rgb{1} = imread(sprintf('DatasetSelection/Original256/%d.jpg', ca_Y{i}.I));
+        annnotation{1} = imread(sprintf('DatasetSelection/Annotations256/%d.png', ca_Y{i}.I));
     else
         original_rgb{1} = imread(sprintf('Dataset2/Original/%d.jpeg', ca_Y{i}.I));
         annnotation{1} = imread(sprintf('Dataset2/Annotations/%d.png', ca_Y{i}.I));
