3c86fcf9538f3d1ed64257f9d80586086d26c451
matejfric <matejfric@centrum.cz>
2023-03-17 18:24:30 +0100

    bug fix in synthetic problem

--- test_synthetic.m
+++ test_synthetic.m
@@ -1,25 +1,34 @@
 clear all
-close all
+%close all
 addpath(genpath(pwd));
+rng(13);
 
-rng(42); %For reproducibility
-
-T = 1e1;
+T = 1000;
 [X_true,Y_true,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem0(T);
+M = size(Y_true, 1); % number of classification classes
 
 sigma = 0.15; % noise parameter
 X = X_true + sigma * randn(size(X_true));
 
 Y = Y_true;
+nwrong = floor(0.05*T); % wrong labels
+rperm = randperm(T);
 
-maxIters = 100;
+for i=1:nwrong
+  idx1 = find(Y(:,rperm(i))==1);
+  idx2 = idx1;
+  while idx1 == idx2
+    idx2 = randi(M);
+  end
+  Y([idx1 idx2],rperm(i)) = Y([idx2 idx1],rperm(i)); % swap rows (labels)
+end
+
+maxIters = 30;
 nrand = 5;
 Ks = size(C_true,2);
 
-%alphas = 0:0.2:1;
 alphas = 0:0.05:1;
 %alphas = 0.05:0.05:0.95;
-%alphas = 0.9:0.01:0.99;
 
 Ls = cell(3,1);
 L1s = cell(3,1);
@@ -35,10 +44,10 @@ for idx_alpha=1:length(alphas)
     
         for idx_K=1:length(Ks)
             K = Ks(idx_K);
-
+            
             [C1, Gamma1, PiX1, Lambda1, it1, stats1, L_out1] = adamar_kmeans(X', Y, K, alpha, maxIters, nrand);
             [C2, Gamma2, PiX2, Lambda2, it2, stats2, L_out2] = adamar_fmincon(X', Y, K, alpha, maxIters, nrand);
-            [C3, Gamma3, PiX3, Lambda3, it3, stats3, L_out3] = adamar_spa(X', Y, K, alpha, maxIters, nrand);
+            %[C3, Gamma3, PiX3, Lambda3, it3, stats3, L_out3] = adamar_spa(X', Y, K, alpha, maxIters, nrand);
 
             Ls{1}(idx_alpha,idx_K)  = L_out1.L;
             L1s{1}(idx_alpha,idx_K) = L_out1.L1;
@@ -48,9 +57,9 @@ for idx_alpha=1:length(alphas)
             L1s{2}(idx_alpha,idx_K) = L_out2.L1;
             L2s{2}(idx_alpha,idx_K) = L_out2.L2;
 
-            Ls{3}(idx_alpha,idx_K)  = L_out3.L;
-            L1s{3}(idx_alpha,idx_K) = L_out3.L1;
-            L2s{3}(idx_alpha,idx_K) = L_out3.L2;
+            %Ls{3}(idx_alpha,idx_K)  = L_out3.L;
+            %L1s{3}(idx_alpha,idx_K) = L_out3.L1;
+            %L2s{3}(idx_alpha,idx_K) = L_out3.L2;
         end
 end
 
@@ -101,24 +110,18 @@ function [X,Y,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem0(T)
 
 rng(42);
 
-K = 4; % clusters
-
+K = 4; % number of clusters
 C_true = randi([-10,10],10,K);
-
-[D,K] = size(C_true);
-  
 Gamma_true = zeros(K,T);
 idx = randi(K,T,1);
 for k = 1:K
     Gamma_true(k,idx == k) = 1; % random Gamma
 end
-
+% M = 3 (classification classes), K = 4 
 Lambda_true = [1 0 0 1;...
                0 1 0 0; ...
                0 0 1 0];
-
 Y = Lambda_true*Gamma_true; % PiY
-
 X = C_true*Gamma_true;
            
 end
