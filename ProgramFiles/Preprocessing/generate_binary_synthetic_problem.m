362c6ee64fe51a176d87b3fef50b19e76b8b026a
matejfric <matejfric@centrum.cz>
2023-04-03 14:26:48 +0200

    change visualization - better plots

--- ProgramFiles/Preprocessing/generate_binary_synthetic_problem.m
+++ ProgramFiles/Preprocessing/generate_binary_synthetic_problem.m
@@ -1,21 +1,21 @@
-function [X,Pi,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem(T)
-%GENERATE_PROBLEM
+function [X,Pi,C_true,Gamma_true,Lambda_true] =...
+    generate_binary_synthetic_problem(T)
+%GENERATE_BINARY_SYNTHETIC_PROBLEM
 
-K = 4; % clusters
+rng(42);
+
+K = 4; % number of clusters
 C_true = randi([-10,10],10,K);
-[D,K] = size(C_true);
-  
 Gamma_true = zeros(K,T);
 idx = randi(K,T,1);
 for k = 1:K
     Gamma_true(k,idx == k) = 1; % random Gamma
 end
 
-Lambda_true = [0.8 0   0   0.5
-               0   1   0   0.4
-               0   0   0.7 0 
-               0.2 0   0.3 0.1];
-
+Lambda_true = [1 0 0 1
+               0 1 0 0
+               0 0 1 0];
+           
 Piprob = Lambda_true*Gamma_true;
 
 edges = cumsum([zeros(1,T);Piprob],1);
@@ -32,6 +32,6 @@ for i = 1:size(Pi,1)
 end
 
 X = C_true*Gamma_true;
+% Pi = Lambda_true*Gamma_true;
            
 end
-
--- test_synthetic.m
+++ test_synthetic.m
@@ -5,31 +5,32 @@ rng(13);
 tic;
 
 T = 1000;
-[X_true,Y_true,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem0(T);
-M = size(Y_true, 1); % number of classification classes
+[X_true,Pi_true,C_true,Gamma_true,Lambda_true] = generate_binary_synthetic_problem(T);
+%[X_true,Pi_true,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem(T);
+M = size(Pi_true, 1); % number of classification classes
 
 sigma = 0.15; % noise parameter
 X = X_true + sigma * randn(size(X_true));
 
-Y = Y_true;
+Pi = Pi_true;
 nwrong = floor(0.05*T); % wrong labels
 rperm = randperm(T);
 
 for i=1:nwrong
-  idx1 = find(Y(:,rperm(i))==1);
+  idx1 = find(Pi(:,rperm(i))==1);
   idx2 = idx1;
   while idx1 == idx2
     idx2 = randi(M);
   end
-  Y([idx1 idx2],rperm(i)) = Y([idx2 idx1],rperm(i)); % swap rows (labels)
+  Pi([idx1 idx2],rperm(i)) = Pi([idx2 idx1],rperm(i)); % swap rows (labels)
 end
 
 maxIters = 50;
-nrand = 3;
+nrand = 4;
 Ks = size(C_true,2);
 
-alphas = 0:0.1:1;
-%alphas = 0.05:0.05:0.95;
+alphas = 0:0.05:1;
+%alphas = 0.5;
 
 Ls = cell(3,1);
 L1s = cell(3,1);
@@ -43,89 +44,100 @@ end
 for idx_alpha=1:length(alphas)
     alpha = alphas(idx_alpha);
     
-        for idx_K=1:length(Ks)
-            K = Ks(idx_K);
-            
-            [C1, Gamma1, PiX1, Lambda1, it1, stats1, L_out1] = adamar_kmeans(X', Y, K, alpha, maxIters, nrand);
-            [C2, Gamma2, PiX2, Lambda2, it2, stats2, L_out2] = adamar_fmincon(X', Y, K, alpha, maxIters, nrand);
-            [C3, Gamma3, PiX3, Lambda3, it3, stats3, L_out3] = adamar_spa(X', Y, K, alpha, maxIters, nrand);
+    for idx_K=1:length(Ks)
+        K = Ks(idx_K);
 
-            Ls{1}(idx_alpha,idx_K)  = L_out1.L;
-            L1s{1}(idx_alpha,idx_K) = L_out1.L1;
-            L2s{1}(idx_alpha,idx_K) = L_out1.L2;
+        [C1, Gamma1, PiX1, Lambda1, it1, stats1, L_out1] = adamar_kmeans(X', Pi, K, alpha, maxIters, nrand);
+        [C2, Gamma2, PiX2, Lambda2, it2, stats2, L_out2] = adamar_fmincon(X', Pi, K, alpha, maxIters, nrand);
+%         [C3, Gamma3, PiX3, Lambda3, it3, stats3, L_out3] = adamar_spa(X', Y, K, alpha, maxIters, nrand);
 
-            Ls{2}(idx_alpha,idx_K)  = L_out2.L;
-            L1s{2}(idx_alpha,idx_K) = L_out2.L1;
-            L2s{2}(idx_alpha,idx_K) = L_out2.L2;
+        Ls{1}(idx_alpha,idx_K)  = L_out1.L;
+        L1s{1}(idx_alpha,idx_K) = L_out1.L1;
+        L2s{1}(idx_alpha,idx_K) = L_out1.L2;
 
-            Ls{3}(idx_alpha,idx_K)  = L_out3.L;
-            L1s{3}(idx_alpha,idx_K) = L_out3.L1;
-            L2s{3}(idx_alpha,idx_K) = L_out3.L2;
-        end
+        Ls{2}(idx_alpha,idx_K)  = L_out2.L;
+        L1s{2}(idx_alpha,idx_K) = L_out2.L1;
+        L2s{2}(idx_alpha,idx_K) = L_out2.L2;
+
+%         Ls{3}(idx_alpha,idx_K)  = L_out3.L;
+%         L1s{3}(idx_alpha,idx_K) = L_out3.L1;
+%         L2s{3}(idx_alpha,idx_K) = L_out3.L2;
+    end
+    
+%     clc % clear command window
+    fprintf('Finished iteration for alpha=%.2f', alpha);
+%     pause(1)
 end
 
+% PLOTS
+lw = 2; % LineWidth
+label_fs = 15;
+axis_fs = 12;
+legend_fs = 14;
+jensen_linestyle = '*-';
+spg_linestyle = 'd-';
+spa_linestyle = 's-';
+newcolors = [0     0.447 0.741 %blue
+             0.85  0.325 0.098 %orange
+             0.466 0.674 0.188 %green
+             ];
 % L-curve
 for idx_K=1:length(Ks)
     figure
+    colororder(newcolors)
     hold on
-    title(sprintf('K=%d', K))
-    plot(L1s{1}(:,idx_K), L2s{1}(:,idx_K),'r-o');
-    plot(L1s{2}(:,idx_K), L2s{2}(:,idx_K),'b-o');
-    plot(L1s{3}(:,idx_K), L2s{3}(:,idx_K),'m-o');
-
-    xlabel('$L_1$','Interpreter','latex')
-    ylabel('$L_2$','Interpreter','latex')
-    legend('jensen','fmincon','spa')
+    %title(sprintf('K=%d', K))
+    plot(L1s{1}(:,idx_K), L2s{1}(:,idx_K),jensen_linestyle(:), 'LineWidth', lw);
+    plot(L1s{2}(:,idx_K), L2s{2}(:,idx_K),spg_linestyle(:), 'LineWidth', lw);
+    plot(L1s{3}(:,idx_K), L2s{3}(:,idx_K),spa_linestyle(:), 'LineWidth', lw);
+    xlabel('$L_1$','Interpreter','latex','FontSize', label_fs)
+    ylabel('$L_2$','Interpreter','latex','FontSize', label_fs)
+    legend('jensen','spg','spa','FontSize', legend_fs)
     hold off
+    grid on
+    grid minor
+    ax = gca;
+    ax.FontSize = axis_fs;
 end
 
 for idx_K=1:length(Ks)
     figure
+    colororder(newcolors)
+    ax = gca;
+    ax.FontSize = axis_fs;
+    
     subplot(1,3,1)
     hold on
-    plot(alphas,Ls{1}(:, idx_K),'r*-')
-    plot(alphas,Ls{2}(:, idx_K),'b*-')
-    plot(alphas,Ls{3}(:, idx_K),'m*-')
-    xlabel('$\alpha$','Interpreter','latex')
-    ylabel('$L$','Interpreter','latex')
-    legend('jensen','fmincon','spa')
+    grid on
+    grid minor
+    plot(alphas,Ls{1}(:, idx_K),jensen_linestyle(:), 'LineWidth', lw)
+    plot(alphas,Ls{2}(:, idx_K),spg_linestyle(:), 'LineWidth', lw)
+    plot(alphas,Ls{3}(:, idx_K),spa_linestyle(:), 'LineWidth', lw)
+    xlabel('$\alpha$','Interpreter','latex','FontSize', label_fs)
+    ylabel('$L$','Interpreter','latex','FontSize', label_fs)
+    legend('jensen','spg','spa','FontSize', legend_fs)
+    
     subplot(1,3,2)
     hold on
-    plot(alphas,L1s{1}(:, idx_K),'r*-')
-    plot(alphas,L1s{2}(:, idx_K),'b*-')
-    plot(alphas,L1s{3}(:, idx_K),'m*-')
-    xlabel('$\alpha$','Interpreter','latex')
-    ylabel('$L_1$','Interpreter','latex')
+    grid on
+    grid minor
+    plot(alphas,L1s{1}(:, idx_K),jensen_linestyle(:), 'LineWidth', lw)
+    plot(alphas,L1s{2}(:, idx_K),spg_linestyle(:), 'LineWidth', lw)
+    plot(alphas,L1s{3}(:, idx_K),spa_linestyle(:), 'LineWidth', lw)
+    xlabel('$\alpha$','Interpreter','latex','FontSize', label_fs)
+    ylabel('$L_1$','Interpreter','latex','FontSize', label_fs)
+    
     subplot(1,3,3)
     hold on
-    plot(alphas,L2s{1}(:, idx_K),'r*-')
-    plot(alphas,L2s{2}(:, idx_K),'b*-')
-    plot(alphas,L2s{3}(:, idx_K),'m*-')
-    xlabel('$\alpha$','Interpreter','latex')
-    ylabel('$L_2$','Interpreter','latex')
+    grid on
+    grid minor
+    plot(alphas,L2s{1}(:, idx_K),jensen_linestyle(:), 'LineWidth', lw)
+    plot(alphas,L2s{2}(:, idx_K),spg_linestyle(:), 'LineWidth', lw)
+    plot(alphas,L2s{3}(:, idx_K),spa_linestyle(:), 'LineWidth', lw)
+    xlabel('$\alpha$','Interpreter','latex','FontSize', label_fs)
+    ylabel('$L_2$','Interpreter','latex','FontSize', label_fs)
     hold off
 end
 
 toc
-
-function [X,Y,C_true,Gamma_true,Lambda_true] = generate_synthetic_problem0(T)
-%GENERATE_PROBLEM
-
-rng(42);
-
-K = 4; % number of clusters
-C_true = randi([-10,10],10,K);
-Gamma_true = zeros(K,T);
-idx = randi(K,T,1);
-for k = 1:K
-    Gamma_true(k,idx == k) = 1; % random Gamma
-end
-% M = 3 (classification classes), K = 4 
-Lambda_true = [1 0 0 1;...
-               0 1 0 0; ...
-               0 0 1 0];
-Y = Lambda_true*Gamma_true; % PiY
-X = C_true*Gamma_true;
-           
-end
 
