c3c19f94c4361431218e32caf146fd89e882b8ee
matejfric <matejfric@centrum.cz>
2023-04-14 13:02:46 +0200

    Add corrosion overlay

--- ProgramFiles/visualize.m
+++ ProgramFiles/visualize.m
@@ -13,19 +13,71 @@ prediction = vector2image(prediction, original_rgb);
 annnotation = cell2mat(annnotation);
 annnotation(annnotation==1) = 255;   
 
-[width, height] = get_screen_resolution();
+%[width, height] = get_screen_resolution();
+%figure('Renderer', 'painters', 'Position', [width/4 height/4 width/2 height/2])
 
-figure('Renderer', 'painters', 'Position', [width/4 height/4 width/2 height/2])
+f=figure;
+f.WindowState = 'maximized';
 sgtitle(caption) 
-subplot(2,2,1)
+fontSize = 15;
+
+tiledlayout(1,5,'TileSpacing','Compact','Padding','Compact');
+% Original image
+nexttile
 imshow(cell2mat(original_rgb))
-subplot(2,2,2)
+title("Original image", 'Interpreter', 'latex', 'FontSize', fontSize)
+% Ground truth
+nexttile
 imshow(annnotation, [])
-subplot(2,2,3)
+title("Ground truth", 'Interpreter', 'latex', 'FontSize', fontSize)
+% Ground truth converted to patches
+nexttile
 imshow(ground_truth, [])
-subplot(2,2,4)
+title("Ground truth patches", 'Interpreter', 'latex', 'FontSize', fontSize)
+% Prediction
+nexttile
 imshow(prediction, [])
+title("Prediction", 'Interpreter', 'latex', 'FontSize', fontSize)
 colorbar
+% Prediction overlay
+nexttile
+irig_img = cell2mat(original_rgb);
+% Crop
+sz = size(irig_img(:,:,1));
+ms = mod(sz,16);
+sz = sz - ms; 
+irig_img = irig_img(1:sz(1),1:sz(2),:);
+imshow(irig_img);
+title("Prediction overlay", 'Interpreter', 'latex', 'FontSize', fontSize)
+red = cat(3, ones(sz), zeros(sz), zeros(sz));
+hold on
+h = imshow(red);
+set(h, 'AlphaData', 0.65 .* prediction(1:sz(1),1:sz(2)))
+
+if false
+    % Original image
+    subplot(n_rows,n_cols,1)
+    imshow(cell2mat(original_rgb))
+    % Ground truth
+    subplot(n_rows,n_cols,2)
+    imshow(annnotation, [])
+    % Ground truth converted to patches
+    subplot(n_rows,n_cols,3)
+    imshow(ground_truth, [])
+    % Prediction
+    subplot(n_rows,n_cols,4)
+    imshow(prediction, [])
+    colorbar
+    % Prediction overlay
+    subplot(n_rows,n_cols, 5)
+    irig_img = cell2mat(original_rgb);
+    imshow(irig_img);
+    sz = size(irig_img(:,:,1));
+    red = cat(3, ones(sz), zeros(sz), zeros(sz));
+    hold on
+    h = imshow(red);
+    set(h, 'AlphaData', 0.75 .* prediction(1:sz(1),1:sz(2)))
+end
 
 end
 
@@ -42,12 +94,4 @@ function img = vector2image(vector, original_image)
     end
     
     img = cell2mat(ca_patches);
-end
-
-function [width, height] = get_screen_resolution()
-    set(0,'units','pixels')
-    res = get(0,'ScreenSize');
-    res = res(3:4);
-    width = res(1);
-    height = res(2);
 end
