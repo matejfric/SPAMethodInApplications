e4801fad93773aa10c3709aff1adf3fa4d0630a5
matejfric <matejfric@centrum.cz>
2023-04-28 21:30:01 +0200

    update grid search

--- ProgramFiles/Adamar/OOP/grid_search_mcc.m
+++ ProgramFiles/Adamar/OOP/grid_search_mcc.m
@@ -1,30 +1,30 @@
-function [best_alpha, best_K, best_score] = grid_search(stats_val,alphas,Ks)
+function [best_alpha, best_K, best_mcc] = grid_search_mcc(stats_val,alphas,Ks)
 %GRID_SEARCH 
 
 % One dimensional grid case 
 if ~(sum(size(stats_val) >= [2,2]) >= 2)
     if length(Ks) > length(alphas)
-        [best_score, k] = max([stats_val.f1score]);
+        [best_mcc, k] = max([stats_val.mcc]);
         best_K = Ks(k);
         best_alpha = alphas;
         
         figure
-        plot(Ks, [stats_val.f1score], 'o-', 'LineWidth', 2)
+        plot(Ks, [stats_val.mcc], 'o-', 'LineWidth', 2)
         hold on
-        plot(best_K, best_score, 'Marker', '^', 'MarkerSize', 15)
+        plot(best_K, best_epsilon, 'Marker', '^', 'MarkerSize', 15)
         xlabel('$K$','interpreter','latex','FontSize',14)
-        ylabel('$F_1$-score','interpreter','latex','FontSize',14)
+        ylabel('MCC','interpreter','latex','FontSize',14)
     else
-        [best_score, a] = max([stats_val.f1score]);
+        [best_mcc, a] = max([stats_val.mcc]);
         best_alpha = alphas(a);
         best_K = Ks;
         
         figure
-        plot(alphas, [stats_val.f1score], 'o-', 'LineWidth', 2)
+        plot(alphas, [stats_val.mcc], 'o-', 'LineWidth', 2)
         hold on
-        plot(best_alpha, best_score, 'Marker', '^', 'MarkerSize', 15)
+        plot(best_alpha, best_mcc, 'Marker', '^', 'MarkerSize', 15)
         xlabel('$\alpha$','interpreter','latex','FontSize',14)
-        ylabel('$F_1$-score','interpreter','latex','FontSize',14)
+        ylabel('MCC','interpreter','latex','FontSize',14)
     end
     return
 end
@@ -33,7 +33,7 @@ end
 % Two dimensional grid case 
 scores = zeros(size(stats_val));
 for j=1:size(scores,2)
-    scores(:,j) = [stats_val(:,j).f1score]';
+    scores(:,j) = [stats_val(:,j).mcc]';
 end
 
 [XX,YY] = meshgrid(alphas,Ks);
@@ -42,15 +42,15 @@ figure
 surfc(XX,YY,scores','FaceColor','interp','FaceAlpha',0.9)
 colorbar;
 colormap jet;
-[i,j,best_score] = max_in_matrix(scores);
+[i,j,best_mcc] = max_in_matrix(scores);
 xlabel('$\alpha$','interpreter','latex')
 ylabel('$K$','interpreter','latex')
-zlabel('$F_1$-score','interpreter','latex') 
+zlabel('MCC','interpreter','latex') 
 hold on
 best_alpha = alphas(i);
 best_K = Ks(j);
-scatter3(best_alpha,best_K,best_score,100,'^k','filled');
-text(best_alpha, best_K, best_score,...
+scatter3(best_alpha,best_K,best_mcc,100,'^k','filled');
+text(best_alpha, best_K, best_mcc,...
     sprintf('($\\alpha$=%.4f, $K$=%d)', best_alpha, best_K),...
     'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom',...
     'interpreter','latex','FontSize',14);
